9b3dd5e47ea66f4de599d324283f1c872db98853
Daniel Navas Borjas <dani@MacBook-Pro-de-Daniel.local>
2024-10-04 19:43:54 +0200

    PHP => Ejercicio 5 Strings

--- PHP/Pract_strings/ejer5/vistas/vista_respuesta.php
+++ PHP/Pract_strings/ejer5/vistas/vista_respuesta.php
@@ -1,31 +1,45 @@
 <div id="verde">
     <h1>Árabes a romanos - Resultado</h1>
     <?php
+        $cont["M"]=4;
+        $cont["D"]=1;
+        $cont["C"]=4;
+        $cont["L"]=1;
+        $cont["X"]=4;
+        $cont["V"]=1;
+        $cont["I"]=4;
         $respuesta = '';
         $temporal_number = $texto1;
 
         while ($temporal_number > 0) {
-            if ($temporal_number > 1000) {
+            if ($temporal_number > 1000 && $cont["M"] > 0) {
                 $temporal_number -= 1000;
                 $respuesta .= 'M';
-            } else if ($temporal_number > 500) {
+                $cont["M"]--;
+            } else if ($temporal_number > 500 && $cont["D"] > 0) {
                 $temporal_number -= 500;
                 $respuesta .= 'D';
-            } else if ($temporal_number > 100) {
+                $cont["D"]--;
+            } else if ($temporal_number > 100 && $cont["C"] > 0) {
                 $temporal_number -= 100;
                 $respuesta .= 'C';
-            } else if ($temporal_number > 50) {
+                $cont["C"]--;
+            } else if ($temporal_number > 50 && $cont["L"] > 0) {
                 $temporal_number -= 50;
                 $respuesta .= 'L';
-            } else if ($temporal_number > 10) {
+                $cont["L"]--;
+            } else if ($temporal_number > 10 && $cont["X"] > 0) {
                 $temporal_number -= 10;
                 $respuesta .= 'X';
-            } else if ($temporal_number > 5) {
+                $cont["X"]--;
+            } else if ($temporal_number > 5 && $cont["V"] > 0) {
                 $temporal_number -= 5;
                 $respuesta .= 'V';
+                $cont["V"]--;
             } else {
                 $temporal_number -= 1;
                 $respuesta .= 'I';
+                $cont["I"]--;
             }
         }
 
