e011caa09349ef6c0a1f00d8a9cf505b4788d809
Daniel Navas Borjas <dani@MacBook-Pro-de-Daniel.local>
2024-10-15 22:25:47 +0200

    PHP => Ejercicio1 ficheros

--- PHP/Pract_ficheros/ejer1/index.php
+++ PHP/Pract_ficheros/ejer1/index.php
@@ -1,6 +1,6 @@
 <?php 
     if (isset($_POST["btnCrear"])) {
-        $error_tabla = $_POST["numero"] = "" || !is_numeric($_POST["numero"]) || $_POST["numero"] < 1 || $_POST["numero"] > 10;
+        $error_tabla = $_POST["numero"] == "" || !is_numeric($_POST["numero"]) || $_POST["numero"] < 1 || $_POST["numero"] > 10;
     }
 ?>
 <!DOCTYPE html>
@@ -21,7 +21,7 @@
         <input type="text" name="numero" id="numero">
         <?php 
             if (isset($_POST["btnCrear"]) && $error_tabla) {
-                if ($_POST["numero"] = "") {
+                if ($_POST["numero"] == "") {
                     echo "<span class='error'>* Campo vacío *</span>";
                 } else {
                     echo "<span class='error'>* Debes introducir un número entre 1 y 10 *</span>";
@@ -38,6 +38,15 @@
             //si no existe el archivo pasado
             if (!file_exists($nombre_archivo)) {
                 @$file = fopen($nombre_archivo, "w");
+
+                if (!$file) {
+                    die("<p>No se ha podido escribir en el archivo</p>");
+                }
+
+                for ($i = 1; $i <= 10; $i++) {
+                    $multiplicacion = $i . " x " . $_POST["numero"] . " = " . $i*$_POST["numero"];
+                    fputs($file, $multiplicacion . ($i == 10 ? '' : PHP_EOL));
+                }
                 fclose($file);
             }
         }
