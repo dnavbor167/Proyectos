7736c4ca6258a7a1e84469a0466b6433da5143c5
dnavbor167 <dnavbor167@g.educaand.es>
2024-12-13 11:12:34 +0100

    REACT => Avance

--- 2-DAW/1º Trimestre/HLC/REACT/RetoClase/src/App.js
+++ 2-DAW/1º Trimestre/HLC/REACT/RetoClase/src/App.js
@@ -28,11 +28,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Botoncillo color={this.state.listaBtn[0]} pos={0} click={(n)=>this.clickBtn(n)} />
-          <Botoncillo color={this.state.listaBtn[1]} pos={1} click={(n)=>this.clickBtn(n)} />
-          <Botoncillo color={this.state.listaBtn[2]} pos={2} click={(n)=>this.clickBtn(n)} />
-          <Botoncillo color={this.state.listaBtn[3]} pos={3} click={(n)=>this.clickBtn(n)} />
-          <Botoncillo color={this.state.listaBtn[4]} pos={4} click={(n)=>this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[0]} contador={this.state.listaNum[0]} pos={0} click={(n)=>this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[1]} contador={this.state.listaNum[1]}  pos={1} click={(n)=>this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[2]} contador={this.state.listaNum[2]}  pos={2} click={(n)=>this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[3]} contador={this.state.listaNum[3]}  pos={3} click={(n)=>this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[4]} contador={this.state.listaNum[4]}  pos={4} click={(n)=>this.clickBtn(n)} />
         </header>
       </div>
     );
