b9ae309838368dfe617fd9917796279355d7bd03
dnavbor167 <dnavbor167@g.educaand.es>
2024-12-13 12:41:16 +0100

    REACT => Avance

--- 2-DAW/1º Trimestre/HLC/REACT/RetoClase/src/App.js
+++ 2-DAW/1º Trimestre/HLC/REACT/RetoClase/src/App.js
@@ -7,8 +7,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      listaBtn: Array(5).fill("secondary"),
+      listaBtn: Array(5).fill("primary"),
       listaNum: Array(5).fill(0),
+
     };
   }
 
@@ -18,21 +19,42 @@ class App extends Component {
     auxBtn[numBtn] = "primary"
     auxNum[numBtn] = auxNum[numBtn] + 1
 
+    
+    auxNum.forEach((n, index) => {
+      if (n === 0) {
+        
+      }
+    })
+
     this.setState({
       listaBtn: auxBtn,
       listaNum: auxNum,
     })
+
+    let decrement = (index) => {
+      if (auxNum[index] > 0) {
+        auxNum[index]--
+        this.setState({ listaNum: auxNum })
+        setTimeout(() => decrement(index), 3000)
+      }
+      
+    }
+
+    setTimeout(() => decrement(numBtn), 3000)
+
+
+
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Botoncillo color={this.state.listaBtn[0]} contador={this.state.listaNum[0]} pos={0} click={(n)=>this.clickBtn(n)} />
-          <Botoncillo color={this.state.listaBtn[1]} contador={this.state.listaNum[1]}  pos={1} click={(n)=>this.clickBtn(n)} />
-          <Botoncillo color={this.state.listaBtn[2]} contador={this.state.listaNum[2]}  pos={2} click={(n)=>this.clickBtn(n)} />
-          <Botoncillo color={this.state.listaBtn[3]} contador={this.state.listaNum[3]}  pos={3} click={(n)=>this.clickBtn(n)} />
-          <Botoncillo color={this.state.listaBtn[4]} contador={this.state.listaNum[4]}  pos={4} click={(n)=>this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[0]} contador={this.state.listaNum[0]} pos={0} click={(n) => this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[1]} contador={this.state.listaNum[1]} pos={1} click={(n) => this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[2]} contador={this.state.listaNum[2]} pos={2} click={(n) => this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[3]} contador={this.state.listaNum[3]} pos={3} click={(n) => this.clickBtn(n)} />
+          <Botoncillo color={this.state.listaBtn[4]} contador={this.state.listaNum[4]} pos={4} click={(n) => this.clickBtn(n)} />
         </header>
       </div>
     );
@@ -41,7 +63,7 @@ class App extends Component {
 
 function Botoncillo(props) {
   return (
-    <Button color={props.color} onClick={()=>props.click(props.pos)}>{props.contador}</Button>
+    <Button color={props.color} onClick={() => props.click(props.pos)}>{props.contador}</Button>
   );
 }
 
