78c6306042639870903c4f0d9bc40e2d2b0df253
Daniel Navas Borjas <dani@MacBook-Pro-de-Daniel.local>
2024-12-27 00:04:10 +0100

    REACT => Repaso

--- 2-DAW/1º Trimestre/HLC/REACT/Botones_Sencillos/src/App.js
+++ 2-DAW/1º Trimestre/HLC/REACT/Botones_Sencillos/src/App.js
@@ -3,46 +3,64 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'reactstrap';
 
 class App extends Component {
-    constructor(props) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listColores: Array(5).fill('secondary'),
+      numeros: 0,
+    };
+  }
 
-        super(props);
-        this.state = {
-            listaColores: Array(5).fill("secondary"),
-            numero: 0,
-        };
-    }
+  handleOnClick(numero) {
+    let copiaColores = this.state.listColores;
+    copiaColores[numero] =
+      copiaColores[numero] === 'danger' ? 'secondary' : 'danger';
+    this.setState({
+      listColores: copiaColores,
+      numeros: copiaColores.filter((e) => e === 'danger').length,
+    });
+  }
 
-    handleOnClick(numButt) {
-        let copListColo = this.state.listaColores
-        //incrementamos en uno 
-        copListColo[numButt] = "danger";
-
-        this.setState({
-            listaColores: copListColo,
-            numero: copListColo.filter(n => n==="danger").length,
-        })
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>{this.state.numero}</h1>
-                    <Botoncillo color={this.state.listaColores[0]} pos={0} increment={(n) => this.handleOnClick(n)} />
-                    <Botoncillo color={this.state.listaColores[1]} pos={1} increment={(n) => this.handleOnClick(n)} />
-                    <Botoncillo color={this.state.listaColores[2]} pos={2} increment={(n) => this.handleOnClick(n)} />
-                    <Botoncillo color={this.state.listaColores[3]} pos={3} increment={(n) => this.handleOnClick(n)} />
-                    <Botoncillo color={this.state.listaColores[4]} pos={4} increment={(n) => this.handleOnClick(n)} />
-                </header>
-            </div>
-        );
-    }
-}
-
-function Botoncillo(props) {
+  render() {
     return (
-        <Button color={props.color} onClick={() => props.increment(props.pos)}></Button>
+      <div className="App">
+        <h1>{this.state.numeros}</h1>
+        <Botoncillo
+          color={this.state.listColores[0]}
+          pos={0}
+          contarNumeros={(n, e) => this.handleOnClick(n)}
+        />
+        <Botoncillo
+          color={this.state.listColores[1]}
+          pos={1}
+          contarNumeros={(n) => this.handleOnClick(n)}
+        />
+        <Botoncillo
+          color={this.state.listColores[2]}
+          pos={2}
+          contarNumeros={(n) => this.handleOnClick(n)}
+        />
+        <Botoncillo
+          color={this.state.listColores[3]}
+          pos={3}
+          contarNumeros={(n) => this.handleOnClick(n)}
+        />
+        <Botoncillo
+          color={this.state.listColores[4]}
+          pos={4}
+          contarNumeros={(n) => this.handleOnClick(n)}
+        />
+      </div>
     );
+  }
+}
+function Botoncillo(props) {
+  return (
+    <Button
+      color={props.color}
+      onClick={() => props.contarNumeros(props.pos)}
+    ></Button>
+  );
 }
 
 export default App;
