be8ee585894954e5c2399815749e28a5cbd3b575
dnavbor167 <dnavbor167@g.educaand.es>
2025-01-13 10:21:31 +0100

    REACT => plantilla

--- 2-DAW/2Trimestre/HLC/plantilla/src/App.js
+++ 2-DAW/2Trimestre/HLC/plantilla/src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Component } from 'react';
 import { Button } from 'reactstrap';
 
-class App extends Component() {
-  render() {
-    return (
-      <div className="App">
-        <Button>
-          Dale
-        </Button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Button>
+        Dale
+      </Button>
+    </div>
+  );
 }
 
 export default App;
