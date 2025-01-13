de511994f6e719c003df827609a5b25200382d26
dnavbor167 <dnavbor167@g.educaand.es>
2025-01-13 10:02:04 +0100

    REACT => plantilla

---
+++ 2-DAW/2Trimestre/HLC/plantilla/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Component } from 'react';
+import { Button } from 'reactstrap';
+
+class App extends Component() {
+  render() {
+    return (
+      <div className="App">
+        <Button>
+          Dale
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
