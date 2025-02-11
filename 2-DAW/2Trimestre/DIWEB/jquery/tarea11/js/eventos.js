8812e3ccd519a02f1b9114ca43966d45c960d4d5
dnavbor167 <dnavbor167@g.educaand.es>
2025-02-11 12:51:12 +0100

    JQuery => Ejercicio 12

--- 2-DAW/2Trimestre/DIWEB/jquery/tarea11/js/eventos.js
+++ 2-DAW/2Trimestre/DIWEB/jquery/tarea11/js/eventos.js
@@ -1,9 +1,9 @@
 $(function () {
     $("div section div.texto").css("display", "none")
     $("div section>div").on("click", function (e) {
-        $(this).next().toggle()
+        $(this).next().stop().slideToggle()
         $(this).children().children().next().toggle()
-        //$(this).find("svg:nth-child(2)").toggle()
+        //$(this).find("svg:nth-child(2)").slideToggle()
     })
 })
 
